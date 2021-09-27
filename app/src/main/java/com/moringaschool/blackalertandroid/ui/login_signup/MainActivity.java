4331fbe02ae41a7039907657583a6d2f8c7f0156
AugustineKadima <kadimasam@gmail.com>
2021-09-27 12:02:07 +0300

    Initial project setup

---
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/login_signup/MainActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.blackalertandroid.ui.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.blackalertandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
