dd9d958bd3741abe8c1c4c06c938ba3a0ad5022f
AugustineKadima <kadimasam@gmail.com>
2021-09-27 23:30:28 +0300

    Add intents to connect activities

--- app/src/main/java/com/moringaschool/blackalertandroid/ui/login_signup2/CreateAccountOne.java
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/login_signup2/CreateAccountOne.java
@@ -2,15 +2,28 @@ package com.moringaschool.blackalertandroid.ui.login_signup2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.moringaschool.blackalertandroid.R;
 
 public class CreateAccountOne extends AppCompatActivity {
+    Button next_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_one);
+
+        next_btn = (Button) findViewById(R.id.next_btn);
+
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateAccountOne.this, CreateAccountTwo.class));
+            }
+        });
     }
 }
