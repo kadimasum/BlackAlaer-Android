7bfd036d1bf7f4faae55c77e509862dffa88b65d
AugustineKadima <kadimasam@gmail.com>
2021-09-27 18:38:29 +0300

    Create login

---
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/login_signup2/CreateAccountTwo.java
@@ -0,0 +1,16 @@
+package com.moringaschool.blackalertandroid.ui.login_signup2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.blackalertandroid.R;
+
+public class CreateAccountTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_two);
+    }
+}
---
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/login_signup2/Login.java
@@ -0,0 +1,16 @@
+package com.moringaschool.blackalertandroid.ui.login_signup2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.blackalertandroid.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
