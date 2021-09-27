e61b73e79bf4346c332891b430ac319720f00e10
ericwafula <ericwathome007@gmail.com>
2021-09-27 15:19:46 +0300

    created location card

---
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/app/MyAlertsActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.blackalertandroid.ui.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.blackalertandroid.R;
+
+public class MyAlertsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_alerts);
+    }
+}
