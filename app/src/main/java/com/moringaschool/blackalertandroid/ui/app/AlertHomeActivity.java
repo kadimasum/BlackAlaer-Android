544cae067e3e0be6ec1ec2afeb5acbe04bb9c21c
ericwafula <ericwathome007@gmail.com>
2021-09-28 22:25:33 +0300

    added nav_view

--- app/src/main/java/com/moringaschool/blackalertandroid/ui/app/AlertHomeActivity.java
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/app/AlertHomeActivity.java
@@ -1,16 +1,31 @@
 package com.moringaschool.blackalertandroid.ui.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.moringaschool.blackalertandroid.R;
 
 public class AlertHomeActivity extends AppCompatActivity {
+
+    // menu variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert_home);
+
+        // <------- HOOKS -------->
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
     }
 }
