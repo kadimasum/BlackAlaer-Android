44b55c40449e8c5b909e10c40c39e0d8735bd3c4
AugustineKadima <kadimasam@gmail.com>
2021-09-30 01:13:59 +0300

    Modify constants

--- app/src/main/java/com/moringaschool/blackalertandroid/ui/constants/constants.java
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/constants/constants.java
@@ -2,5 +2,5 @@ package com.moringaschool.blackalertandroid.ui.constants;
 
 public class constants {
     public static final String USER_EMAIL = "userEmail";
-    public static final String USER_PASSWORD = "userPassword"
+    public static final String USER_PASSWORD = "userPassword";
 }
