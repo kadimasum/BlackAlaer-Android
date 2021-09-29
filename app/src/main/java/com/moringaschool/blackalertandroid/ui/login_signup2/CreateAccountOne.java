b1710530f38e65a5180acb06d63bf1b63119170a
AugustineKadima <kadimasam@gmail.com>
2021-09-30 00:16:16 +0300

    Validate create account one

--- app/src/main/java/com/moringaschool/blackalertandroid/ui/login_signup2/CreateAccountOne.java
+++ app/src/main/java/com/moringaschool/blackalertandroid/ui/login_signup2/CreateAccountOne.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.moringaschool.blackalertandroid.R;
 
 public class CreateAccountOne extends AppCompatActivity {
     Button next_btn;
     TextView loginLink;
+    EditText first_name, last_name, email, password, confirm_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,53 @@ public class CreateAccountOne extends AppCompatActivity {
 
         next_btn = (Button) findViewById(R.id.next_btn);
         loginLink = (TextView) findViewById(R.id.login_link);
+        first_name = (EditText) findViewById(R.id.Create_account_first_name);
+        last_name = (EditText) findViewById(R.id.Create_account_last_name);
+        email = (EditText)  findViewById(R.id.Create_account_email);
+        password = (EditText) findViewById(R.id.Create_account_password);
+        confirm_password = (EditText) findViewById(R.id.Create_account_confirm_password);
 
         next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(CreateAccountOne.this, CreateAccountTwo.class));
+                String firstName = first_name.getText().toString().trim();
+                String lastName = last_name.getText().toString().trim();
+                String Email = email.getText().toString().trim();
+                String Password = password.getText().toString().trim();
+                String confirmPassword = confirm_password.getText().toString().trim();
+
+
+                if(firstName.isEmpty() && lastName.isEmpty() && Email.isEmpty() && Password.isEmpty() && confirmPassword.isEmpty()){
+                    Toast.makeText(CreateAccountOne.this, "Fill all inputs to proceed", Toast.LENGTH_LONG).show();
+                }
+                else if(firstName.isEmpty()){
+                    first_name.setError("First name required!");
+                    first_name.requestFocus();
+                }else if(lastName.isEmpty()){
+                    last_name.setError("Last name required!");
+                    last_name.requestFocus();
+                }else if(Email.isEmpty()){
+                    email.setError("Email required!");
+                    email.requestFocus();
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                    email.setError("Use the correct email format!");
+                    email.requestFocus();
+                }else if(Password.isEmpty()){
+                    password.setError("Password required!");
+                    password.requestFocus();
+                }else if(confirmPassword.isEmpty()){
+                    confirm_password.setError("Password required!");
+                    confirm_password.requestFocus();
+
+                }else if(Password.length() < 6){
+                    password.setError("Password must be a minimum of six characters");
+                    password.requestFocus();
+                }else if(!(confirmPassword.matches(Password))){
+                    confirm_password.setError("Password does not match");
+                    confirm_password.requestFocus();
+                }else {
+                    startActivity(new Intent(CreateAccountOne.this, CreateAccountTwo.class));
+                }
             }
         });
 
